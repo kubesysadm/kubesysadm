aa8867ae5d03b0c95b9321e76d70bf14157815d0
wangyysde <net_use@bzhy.com>
2024-05-20 13:35:58 +0800

    add manifests verify
    Signed-off-by: wangyysde <net_use@bzhy.com>

--- hack/verify-gencode.sh
+++ hack/verify-gencode.sh
@@ -20,7 +20,9 @@ set -o pipefail
 
 SCRIPT_ROOT=$(unset CDPATH && cd $(dirname "${BASH_SOURCE[0]}")/.. && pwd)
 DIFFROOT="${SCRIPT_ROOT}/api"
+DIFFMANIFESTSROOT="${SCRIPT_ROOT}/config"
 TMP_DIFFROOT="${SCRIPT_ROOT}/_tmp"
+TMP_DIFFMANIFESTSROOT="${TMP_DIFFROOT}/config"
 CONTROLLER_TOOLS_VERSION="v0.14.0"
 CONTROLLER_GEN=${SCRIPT_ROOT}/bin/controller-gen-${CONTROLLER_TOOLS_VERSION}
 DEFINED_PACKAGE_LIST="v1beta1"
@@ -34,19 +36,31 @@ trap "cleanup" EXIT SIGINT
 
 cleanup
 
-mkdir -p ${TMP_DIFFROOT}
-cp -a "${DIFFROOT}"/* "${TMP_DIFFROOT}"
+mkdir -p "${TMP_DIFFROOT}/api"
+mkdir -p "${TMP_DIFFROOT}/config"
+cp -a "${DIFFROOT}"/* "${TMP_DIFFROOT}/api"
 cd ${SCRIPT_ROOT}
 
 . ${SCRIPT_ROOT}/hack/update-gencode.sh
 echo "diffing ${DIFFROOT} against freshly generated codegen"
 ret=0
-diff -Naupr "${DIFFROOT}" "${TMP_DIFFROOT}" || ret=$?
+diff -Naupr "${DIFFROOT}" "${TMP_DIFFROOT}/api" || ret=$?
 if [[ $ret -eq 0 ]]
 then
   echo "${DIFFROOT} up to date."
 else
   echo "${DIFFROOT} is out of date. Please run hack/update-gencode.sh"
+  exit 1
+fi
+
+echo "diffing manifests  against freshly generated codegen"
+ret=0
+diff -Naupr "${DIFFMANIFESTSROOT}/crd/bases" "${TMP_DIFFMANIFESTSROOT}/crd/bases" || ret=$?
+if [[ $ret -eq 0 ]]
+then
+  echo "manifests up to date."
+else
+  echo "manifests is out of date. Please run hack/update-gencode.sh"
   exit 1
 fi
 
