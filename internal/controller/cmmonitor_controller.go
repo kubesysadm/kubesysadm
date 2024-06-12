add2ccedbcd841854cf6afc3321124abd549e075
wangyysde <net_use@bzhy.com>
2024-06-12 14:54:17 +0800

    fix verify codes error
    Signed-off-by: wangyysde <net_use@bzhy.com>

--- internal/controller/cmmonitor_controller.go
+++ internal/controller/cmmonitor_controller.go
@@ -18,13 +18,13 @@ package controller
 
 import (
 	"context"
+	"strings"
 	
 	kappsv1 "k8s.io/api/apps/v1"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
-	"strings"
 
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
